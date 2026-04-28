cc6cc7be3b85fb5395866b3ee0d07cb5fb52b97c
Vincentius Filbert Amadeo <vincentius.filbert@ui.ac.id>
2026-04-28 20:54:33 +0700

    fix: optimize all student name and highest gpa

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -2,11 +2,19 @@ package com.advpro.profiling.tutorial.repository;
 
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findTopByOrderByGpaDesc();
+
+    @Query("select s.name from Student s")
+    List<String> findAllNames();
 }
